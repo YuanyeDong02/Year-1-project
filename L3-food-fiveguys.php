1a69ce01ceebb9702c4d485adcbfa94ddf1e5696
Yining <yining.du@student.manchester.ac.uk>
2022-03-25 12:59:24 +0000

    name changed&part of backend added

---
+++ L3-food-fiveguys.php
@@ -0,0 +1,232 @@
+<html>
+<head>
+    <title>Five Guys</title>
+    <meta content="width=device-width, initial-scale=1" name="viewport">
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
+    <meta content="aaaaa" name="keywords"/>
+    <script type="application/x-javascript"> addEventListener("load", function () {
+        setTimeout(hideURLbar, 0);
+    }, false);
+
+    function hideURLbar() {
+        window.scrollTo(0, 1);
+    } </script>
+
+    <link href="css/bootstrap.css" media="all" rel='stylesheet' type='text/css'/>
+
+
+    <link href="css/style.css" media="all" rel='stylesheet' type='text/css'/>
+    <script src="js/jquery-1.8.3.min.js"></script>
+    <script src="js/modernizr.custom.js"></script>
+
+
+    <link href='http://fonts.useso.com/css?family=Open+Sans:400italic,600italic,700italic,400,300,600,700,800'
+          rel='stylesheet' type='text/css'>
+    <link href='http://fonts.useso.com/css?family=Niconne' rel='stylesheet' type='text/css'>
+
+    <link href="css/component.css" rel="stylesheet" type="text/css"/>
+</head>
+<body class="cbp-spmenu-push">
+
+
+
+<div class="header-section2">
+    <div class="container">
+        <div class="header-top">
+            <div class="top-nav"></div>
+            <div class="header-logo">
+                <a href="index.html"><img alt="" src="images/logo.png"/></a>
+            </div>
+            <div class="search-form">
+                <form>
+                    <input onblur="if (this.value == '') {this.value = 'Search...';}" onfocus="this.value = '';" required=""
+                           type="text" value="Search...">
+                </form>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</div>
+
+
+<div class="blog">
+    <div class="container">
+        <div class="blog-content">
+            <div class="container">
+                <div class="blog-content-left">
+                    <div class="blog-articals">
+                        <div class="blog-artical">
+                            <div class="blog-artical-info">
+                                <ul class="product-head">
+                                    <li><a href="index.html">Home</a> <span>::</span></li>
+                                    <li class="active-page">Five Guys</li>
+                                    <div class="clear"></div>
+                                </ul>
+
+                                <div class="blog-artical-info-head single-head">
+                                    <h2>Five Guys</h2>
+
+                                </div>
+                                <div class="blog-artical-info-img single-head-img">
+                                    <img alt="image" src="images/blog3.jpg">
+                                </div>
+
+                                <div class="blog-artical-info-text">
+                                    HISTORY:<br>
+                                    <BR>
+                                    1986: The first Five Guys location opens in Arlington, VA.<br>
+                                    1986 - 2001: Five Guys opens five locations around the DC metro-area and perfected
+                                    their business of making burgers… and starts to build a cult-like following.<br>
+                                    2002: Five Guys decides DC metro-area residents shouldn't be the only ones to
+                                    experience their burgers and start to franchise in Virginia and Maryland.<br>
+                                    2003: Five Guys sells out of franchise territory within 18 months and starts to open
+                                    the rest of the country for franchise rights.<br>
+                                    2003 - 2012: Five Guys expands to over 1,000 locations in the U.S. and Canada.<br>
+                                    In July 2013, Five Guys opens the first location outside of North America in London,
+                                    England.<br>
+                                    Our International HQ in Amsterdam, NL opened in 2016.<br>
+                                    Today, we have more than 1,500 locations open in North America, Canada, UK,, Middle
+                                    East, Europe and growing.<br>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                        <div class="reply">
+                            <h4>Leave Your Reply</h4>
+                            <form method="POST">
+                                <input onblur="if (this.value == '') {this.value = 'Name';}" onfocus="this.value = '';" type="text" id="username" name="username" value="Name">
+                                <input onblur="if (this.value == '') {this.value = 'Email';}" onfocus="this.value = '';" type="text" id="email" name="email" value="Name">
+                                <textarea onblur="if (this.value == '') {this.value = 'Reply';}" onfocus="this.value = '';" type="text" id="content" name="content">Comment</textarea>
+                                <input type="submit" value="Submit"/>
+                            </form>
+
+                            <!--
+                            <form method="ABC">
+                                <div class="rate">
+                                    <input type="radio" id="star5" name="rate" value="5" />
+                                    <label for="star5" title="rate">5 stars</label>
+                                    <input type="radio" id="star4" name="rate" value="4" />
+                                    <label for="star4" title="rate">4 stars</label>
+                                    <input type="radio" id="star3" name="rate" value="3" />
+                                    <label for="star3" title="rate">3 stars</label>
+                                    <input type="radio" id="star2" name="rate" value="2" />
+                                    <label for="star2" title="rate">2 stars</label>
+                                    <input type="radio" id="star1" name="rate" value="1" />
+                                    <label for="star1" title="rate">1 star</label>
+                                </div>
+                            </form>
+                            -->
+                        </div>
+                        
+                        
+                        <?php
+                            function registerComment(){
+                                $un = $_POST['username'];
+                                $em = $_POST['email'];
+                                $cm = $_POST['content'];
+                                $time = date('Y-m-d H:i:s');
+
+                                $sql = "INSERT INTO fiveguys (username, email, content, commentTime)
+                                        VALUES (:userId, :userEmail, :userContent, :userCommentTime)";
+
+                                $pdo = new pdo('mysql:host=localhost;dbname=mmyyddbb', 'root', '12345687');
+                                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+                                $stmt = $pdo->prepare($sql);
+                                $stmt->execute([
+                                    'userId' => $un,
+                                    'userEmail' => $em,
+                                    'userContent' => $cm,
+                                    'userCommentTime' => $time
+                                ]);
+                            }
+
+                            function showComments(){
+                                        
+                                $sql = "SELECT * FROM fiveguys ORDER BY commentTime DESC";
+                            
+                                $pdo = new pdo('mysql:host=localhost;dbname=mmyyddbb', 'root', '12345687');
+                                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+                                
+                                foreach ($pdo->query($sql) as $row){
+                                    echo '<div class="comments-main">';
+                                    echo '  <div class="col-md-10 cmts-main-right">';
+                                    echo ("     <h5>{$row['username']}&nbsp&nbsp&nbsp&nbsp{$row['email']}</h5>");
+                                    echo ("     <p>{$row['content']}");
+                                    echo '      <div class="cmts">';
+                                    echo '          <div class="col-md-6 cmnts-left">';
+                                    echo ("              <p>{$row['commentTime']}</p>");
+                                    echo '          </div>';
+                                    echo '          <div class="clearfix"></div>';
+                                    echo '      </div>';
+                                    echo '  </div>';
+                                    echo '  <div class="clearfix"></div>';
+                                    echo '</div>';
+                                }
+                            }
+                        ?>
+
+                        <div class="comments">
+                            <h4>COMMENTS</h4>
+                                    <?php
+                                        if (empty($_POST)){
+                                            showComments();
+                                        }
+                                        else{
+                                            registerComment();
+                                            showComments();
+                                        }
+                                    ?>
+                                <div class="clearfix"></div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+                <div class="blog-content-right">
+
+
+                    <div class="menu">
+                        <h3>RECOMMEND</h3>
+                        <br>
+                        <b>DOGS</b><br>
+                        hot dogs<BR>
+                        cheese dogs<br>
+                        <br>
+                        <b>BURGERS</b><br>
+                        hamburger<br>
+                        cheeseburger<br>
+                        little hamburger<br>
+                        little cheeseburger<br>
+                        <br>
+                        <b>SANDWICHES</b><br>
+                        veggie sandwich<br>
+                        cheese veggie sandwich<br>
+                        grilled cheese<br>
+                    </div>
+
+                    <div class="detials">
+                        <h3>DETAILS</h3>
+                        <b>menu</b><br>
+                        https://fiveguys.co.uk/menu/<br>
+                        <br>
+                        <b>address</b><br>
+                        University Green, M13 9GP<br>
+                        <br>
+                        <b>contect us</b><br>
+                        +441612733622
+
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+</body>
+</html>
